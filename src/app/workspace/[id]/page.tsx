8a7ab9ee6c3b0d0619f69340ba493369039a5e15
Shahen Algoo <shahenalgoo@gmail.com>
2023-06-13 22:57:15 +0400

    editor merge conflict

--- src/app/workspace/[id]/page.tsx
+++ src/app/workspace/[id]/page.tsx
@@ -27,7 +27,7 @@ import NoteActionBar from "../components/editor-action-bar/NoteActionBar";
 // Text editor
 import TextareaAutosize from 'react-textarea-autosize';
 import CodeEditor from "../components/editor-code/CodeEditor";
-import TextEditor from "../components/editor-text/TextEditor";
+import TextEditor from "../components/editor/text-editor/TextEditor";
 
 // Utils
 import LoadingComponent from "@/components/misc/Loading";
